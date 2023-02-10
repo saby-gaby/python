67a05f77c3bee279e1ea361dd2ee8d09f1e32e7f
saby-gaby <sgudzenova@gmail.com>
2023-02-10 16:04:18 +0100

    string-methods

--- youTube.py
+++ youTube.py
@@ -31,6 +31,32 @@ age=37
 
 # String Methods
 s='hello world'
-print(s.capitalize())
-print(s.swapcase())
-print(s.replace('world', 'everyone'))
+# Capitalize string
+print(s.capitalize(), '-> capitalized')
+# Make all uppercase
+print(s.upper(), '-> uppercase')
+# Make all lower
+print(s.lower(), '-> lowercase')
+# Swap case
+print(s.swapcase(), '-> swap case')
+# Get length
+print(len(s), '-> length')
+# Replace 
+print(s.replace('world', 'everyone'), '-> replace')
+#Count
+sub='h'
+print(s.count(sub), '-> count "h"')
+#Starts with
+print(s.startswith('hello'), '-> starts with "hello"')
+# Ends with
+print(s.endswith("d"), '-> ends with "d"')
+# Split into a list
+print(s.split(), '-> split')
+# Find position
+print(s.find('r'), '-> position of "r"')
+# Is all alphanumeric
+print(s.isalnum(), "-> all alphanumeric?")
+# Is all alphabetic
+print(s.isalpha(), '-> all alphabetic?')
+# Is all numeric
+print(s.isnumeric(), '-> all numeric?')
