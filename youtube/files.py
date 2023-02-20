cf2ce33d04828a0b8353d1d0cb93592ee2aedeff
saby-gaby <sgudzenova@gmail.com>
2023-02-20 14:29:25 +0100

    files

---
+++ youtube/files.py
@@ -0,0 +1,23 @@
+# Python has functions for creating, reading, updating and deleting files.
+
+# Open a file
+myFile = open("myfile.txt", 'w')
+
+# Get some info
+print('Name: ', myFile.name)
+print('Is Closed: ', myFile.closed)
+print('Opening Mode: ', myFile.mode)
+
+# Write to file
+myFile.write('I love Python')
+myFile.write(' and JavaScript.')
+myFile.close()
+
+# Append to file
+myFile = open("myfile.txt", 'a')
+myFile.write(' I also like React.')
+
+# Read from file
+myFile = open("myfile.txt", 'r+')
+text =  myFile.read(33)
+print(text)
