5d6f4d85395b94fee7e379f1b3cba5b41057ee94
saby-gaby <sgudzenova@gmail.com>
2023-02-24 12:26:29 +0100

    numerical functions

---
+++ first_book/math_operations/digit_functions.py
@@ -0,0 +1,24 @@
+print(abs(-5), abs(-7.5), " => abs")
+
+print(bin(0), bin(333), " => bin")
+
+a = 25
+b = 4
+print(divmod(a, b), " => divmod")  # 25/4 без остатък, остатък
+
+print(float("2.2"), " => float")
+
+print(hex(129), " => hex")
+
+print(int(5.5), int("50", 10), int("0xfff", 16), " => int")
+
+print(max(4, 7, 5), min(1, 4, 9), " => max & min")
+
+print(oct(15), " => oct")
+
+print(pow(5,2), " => pow")
+print(pow(10,2,2), " => pow then %")
+
+print(round(0.33),round(1.7),round(2.658,2), " => round")
+
+print(sum([1,2,3]),sum([1,2,3],1), " => sum")
