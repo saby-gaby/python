bde54c53d7f7b011b2c31372254f2b84d1346faf
saby-gaby <sgudzenova@gmail.com>
2023-02-24 13:37:13 +0100

    math functions

---
+++ first_book/math_operations/math_functions.py
@@ -0,0 +1,13 @@
+import math
+
+print(math.pi)
+print(math.e)
+
+print(math.atan2(2,4))
+print(math.ceil(5.23)) 
+print(math.exp(2))  # math.e * math.e
+print(math.fabs(-56))  # like math.abs()
+print(math.floor(5.99))
+print(math.fmod(7,3)) # like %
+print(math.modf(2.568))
+print(math.sqrt(36))
