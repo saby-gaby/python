db3ed36c54f98c860779f1d2f8523c55a72ac9f2
saby-gaby <sgudzenova@gmail.com>
2023-03-06 09:45:01 +0100

    projects creation

---
+++ softUni-pb/firstSteps/projects_creation.py
@@ -0,0 +1,9 @@
+# Read user input
+architect = input()
+projects_count = int(input())
+
+# Logic
+projects_time = projects_count * 3
+
+# Output
+print(f'The architect {architect} will need {projects_time} hours to complete {projects_count} project/s.')
