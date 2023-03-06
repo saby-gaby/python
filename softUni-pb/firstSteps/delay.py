46b69e1d3cc6637e80d17a19699632bca4353514
saby-gaby <sgudzenova@gmail.com>
2023-03-06 09:46:09 +0100

    delay function

---
+++ softUni-pb/firstSteps/delay.py
@@ -0,0 +1,20 @@
+from time import sleep
+
+print('Hello')
+
+sleep(0.5)
+
+print('How are you?')
+
+sleep(0.5)
+
+print("Bye!")
+
+sleep(1)
+
+print('Hello! ', end='')
+sleep(1.5)
+print('How are you? ', end='')
+sleep(1.5)
+print("Bye!")
+print('huhu-buhu')
