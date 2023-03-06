c35f70d672d72e9b9bb7ffc7ab6caaeb36ccf707
saby-gaby <sgudzenova@gmail.com>
2023-03-06 09:45:43 +0100

    yard greening

---
+++ softUni-pb/firstSteps/yard_greening.py
@@ -0,0 +1,12 @@
+# Input
+area = float(input())
+
+# Logic
+price = area * 7.61
+discount = price * 0.18
+total_price = price - discount
+
+# Output
+print(f'The final price is: {total_price} lv.')
+print(f'The discount is: {discount} lv.')
+
