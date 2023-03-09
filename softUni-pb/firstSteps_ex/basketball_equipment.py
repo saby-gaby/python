9ce13e1eae7290bc8d504289919ee7599adf5792
saby-gaby <sgudzenova@gmail.com>
2023-03-09 11:24:22 +0100

    basketball equipment

---
+++ softUni-pb/firstSteps_ex/basketball_equipment.py
@@ -0,0 +1,10 @@
+fee = int(input())
+
+sneakers = fee * 0.60
+clothes = sneakers * 0.8
+ball = clothes / 4
+accessories = ball / 5
+
+total_costs = fee + sneakers + clothes + ball + accessories
+
+print(total_costs)
