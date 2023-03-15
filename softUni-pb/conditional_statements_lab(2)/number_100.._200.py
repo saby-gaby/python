0a99e38b89285d57df4b89f9131f4b655b1aceee
saby-gaby <sgudzenova@gmail.com>
2023-03-15 15:04:13 +0100

    combine logical operations

--- softUni-pb/conditional_statements_lab(2)/number_100.._200.py
+++ softUni-pb/conditional_statements_lab(2)/number_100.._200.py
@@ -2,7 +2,7 @@ number = int(input())
 
 if number < 100:
     print('Less than 100')
-elif number <= 200:
+elif 200 >= number >= 100:      # number >= 100 and number <= 200
     print('Between 100 and 200')
 else:
     print('Greater than 200')
