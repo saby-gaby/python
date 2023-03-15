98076e86d31c81029832beb559242e605d4f6bf8
saby-gaby <sgudzenova@gmail.com>
2023-03-15 08:55:18 +0100

    area of figures

---
+++ softUni-pb/conditional_statements_lab(2)/area_of_figures.py
@@ -0,0 +1,21 @@
+import math
+
+figure = input()
+area = 0
+
+if figure == 'square':
+    side = float(input())
+    area = math.pow(side, 2)
+elif figure == 'rectangle':
+    side_a = float(input())
+    side_b = float(input())
+    area = side_a * side_b
+elif figure == 'circle':
+    r = float(input())
+    area = math.pi * math.pow(r, 2)
+elif figure == 'triangle':
+    side = float(input())
+    h = float(input())
+    area = side * h / 2
+
+print(format(area, '.3f'))
