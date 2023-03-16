0bccf3e2b125a2cf8f6f261ad9df63c007617016
saby-gaby <sgudzenova@gmail.com>
2023-03-16 10:29:05 +0100

    time + 15 minutes

---
+++ softUni-pb/conditional_statements_ex(2)/time+15_minutes.py
@@ -0,0 +1,14 @@
+hours = int(input())
+minutes = int(input()) + 15
+
+if minutes > 59:
+    hours += 1
+    minutes -= 60
+
+if minutes < 10:
+    minutes = '0' + str(minutes)
+
+if hours > 23:
+    hours = 0
+
+print(f'{hours}:{minutes}')
