75d678d26d6c2193e6ff940b509aeab34831d1f5
saby-gaby <sgudzenova@gmail.com>
2023-05-18 14:17:52 +0200

    integer operations - task

---
+++ softUni_fundamentals/data_types_and_variables_ex/integer_operations.py
@@ -0,0 +1,8 @@
+first_num = int(input())
+second_num = int(input())
+third_num = int(input())
+last_num = int(input())
+
+result_num = int((first_num + second_num) / third_num) * last_num
+
+print(result_num)
