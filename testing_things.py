f432acb37477821995e9d960c21bc01d6735ff69
saby-gaby <sgudzenova@gmail.com>
2023-05-23 09:10:43 +0200

    directory name fixed

--- testing_things.py
+++ testing_things.py
@@ -1,10 +1,19 @@
-remaining_days = int(input())
-m = int(input())
+# remaining_days = int(input())
+# m = int(input())
+#
+# # for num in range(m, n, -1):
+# #     if num % n == 0:
+# #         print(num)
+# #         break
+#
+# total_spirit = 0 if remaining_days % 10 != 0 else -30
+# print(total_spirit)
 
-# for num in range(m, n, -1):
-#     if num % n == 0:
-#         print(num)
-#         break
+lst = ['coffee', 'tea', 'water']
+print(lst.index('tea'))
 
-total_spirit = 0 if remaining_days % 10 != 0 else -30
-print(total_spirit)
+is_even = lambda num: num % 2
+if is_even(4):
+    print('odd')
+else:
+    print('even')
