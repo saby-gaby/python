2ded43335051a18a63952748eeb59cbd939418e5
saby-gaby <sgudzenova@gmail.com>
2023-05-31 11:17:21 +0200

    calculate rectangle area - task

---
+++ softUni_fundamentals/functions_lab/rectangle_area.py
@@ -0,0 +1,10 @@
+def calculate_area(side_a, side_b):
+    return side_a * side_b
+
+
+side_a_len = int(input())
+side_b_len = int(input())
+print(calculate_area(side_a_len, side_b_len))
+
+# area = lambda a, b: a * b
+# print(area(side_a_len, side_b_len))
