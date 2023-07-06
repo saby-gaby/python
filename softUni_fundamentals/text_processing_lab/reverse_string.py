f0cc358709660252d414bee23bf0a732184119df
saby-gaby <sgudzenova@gmail.com>
2023-07-06 14:17:28 +0200

    reverse_string.py

---
+++ softUni_fundamentals/text_processing_lab/reverse_string.py
@@ -0,0 +1,16 @@
+string = input()
+
+while string != 'end':
+    print(f'{string} = {string[::-1]}')
+    string = input()
+
+# tests
+# helLo
+# Softuni
+# bottle
+# end
+
+# Dog
+# caT
+# chAir
+# end
